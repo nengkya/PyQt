3041a6d4c08cdfc6ee96540fdfa4096c17f5756b
Your Name <you@example.com>
2018-02-11 16:00:45 +0700

    Sambil nge-Uber motor :v

--- QDateTimeEdit.py
+++ QDateTimeEdit.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 
 class MainForm(QtWidgets.QWidget):
     def __init__(self):
@@ -13,11 +13,17 @@ class MainForm(QtWidgets.QWidget):
 
         dateLabel = QtWidgets.QLabel()
         dateLabel.setText('Tanggal')
-        self.dateEdit = QtWidgets.QDateEdit()
+        self.dateEdit = QtWidgets.QDateEdit() #dd/mm/yy
+        self.dateEdit.setDisplayFormat('dddd dd/MM/yyyy') #dddd/mm/yyyy error
+        self.dateEdit.setDate(QtCore.QDate.currentDate())
+
+        timeLabel = QtWidgets.QLabel()
+        timeLabel.setText('Waktu')
 
         layout = QtWidgets.QGridLayout()
         layout.addWidget(dateLabel)
         layout.addWidget(self.dateEdit)
+        layout.addWidget(timeLabel)
         self.setLayout(layout)
 
 
